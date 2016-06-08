a6613d5ec692df020f2b38d2c9213cd85b06ac23
Lawrence Norton <lawrence@lawrencenorton.com>
2016-06-08 11:30:52 -0400

    simple ajax

--- app/layouts/mainLayout.jsx
+++ app/layouts/mainLayout.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router'
+import ajax from 'superagent'
 
 class MainLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'lawrence',
+      commits: []
+    };
+  }
+  componentWillMount() {
+    ajax.get('https://api.github.com/repos/facebook/react/commits')
+        .end((error, response) => {
+            if (!error && response) {
+                console.log(response.body)
+                this.setState({ commits: response.body });
+            } else {
+                console.log('There was an error fetching from GitHub', error);
+            }
+        }
+    );
+  }
   render() {
 
     return (
@@ -11,6 +31,13 @@ class MainLayout extends React.Component {
           <li><Link to="/about">About</Link></li>
           <li><Link to="/">Home</Link></li>
         </ul>
+        <div>
+          {this.state.commits.map((commit, index) => (
+            <p key={index}>
+              <strong>{commit.author.login}</strong>:
+              <a href={commit.html_url}>{commit.commit.message}</a>.</p>
+          ))}
+        </div>
         {this.props.children}
       </div>
 
